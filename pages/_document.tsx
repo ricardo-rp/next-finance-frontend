5bae680b96e88442ae7b67415da833494608a5ee
moro <ricardoromero4444@gmail.com>
2022-06-02 21:44:55 -0300

    ⏪ : Revert to basic picocss setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css"
+          />
         </Head>
         <body>
           <Main />
